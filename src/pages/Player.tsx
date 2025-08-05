a8d9ac7b12a354c628c21efc90dd3cb6e8b0845d
palmaisaias <palmaisaias@yahoo.com>
2025-08-05 00:35:41 -0700

    soundy sound

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -4,7 +4,7 @@
 import React, { useEffect } from 'react';
 import { Box, Button, Flex, Spinner, Text, VStack } from '@chakra-ui/react';
 
-const VIDEO_SRC = 'https://lemon-squeezy.sfo3.cdn.digitaloceanspaces.com/lemon.mp4';
+const VIDEO_SRC = 'https://lemon-squeezy.sfo3.cdn.digitaloceanspaces.com/lemon_crf25.mp4';
 
 export default function Player() {
   useEffect(() => {
